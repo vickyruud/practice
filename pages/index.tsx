665f44041e20d06d57d9a37f82091f9cfa7e070a
vickyruud <aprajit.k@gmail.com>
2022-08-21 11:11:57 -0400

    add tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
   const fetchMovies: any = () => {
     return axios
       .get(
-        "https://api.themoviedb.org/3/movie/popular?api_key=12f5be9e44863cafc7c5762441a2017a&language=en-US&page=1"
+        "https://api.themoviedb.org/3/movie/popular?api_key=12f5be9e44863cafc7c5762441a2017a&language=en-US&page=10"
       )
       .then((res) => {
         setMovies(res.data.results);
@@ -22,12 +22,12 @@ const Home: NextPage = () => {
     fetchMovies();
   }, []);
 
-  const renderMovies = movies.map((movie) => {
-    return <p>{movie.title}</p>;
+  const renderMovies = movies.map((movie, i) => {
+    return <p key={i}>{movie.title}</p>;
   });
 
   return (
-    <div>
+    <div className="m-5">
       Hello
       {movies && renderMovies}
     </div>
